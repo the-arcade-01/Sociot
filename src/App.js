cad5d014b2c53ae31ceb3c0170edae74bbe6f102
the-arcade-01 <aashisharcade@gmail.com>
2022-01-12 23:14:47 +0530

    drawer started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-function App() {
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#f7f7f7",
+    },
+  },
+  typography: {
+    fontFamily: "Nunito",
+  },
+  shadows: Array(25).fill("none"),
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
