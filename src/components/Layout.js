00036831c9a4d8956f735948fea1f19a8699ade9
the-arcade-01 <aashisharcade@gmail.com>
2022-01-17 21:38:49 +0530

    files added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,35 @@
 import React from "react";
 
+import { Typography, Drawer } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const drawerWidth = 300;
+
+const useStyles = makeStyles({
+  root: {
+    color: "#000",
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+});
+
 const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>From the layout</h1>
-      {children}
+    <div className={classes.root}>
+      <Drawer
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <Typography variant="h5" sx={{ color: "#000" }}>
+          Meowth
+        </Typography>
+      </Drawer>
     </div>
   );
 };
