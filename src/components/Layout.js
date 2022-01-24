763574920e7bed87e1781d7359b4ce6b5d033069
the-arcade-01 <aashisharcade@gmail.com>
2022-01-24 09:17:25 +0530

    added layout 50% done

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,10 +3,20 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import Avatar from "@mui/material/Avatar";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 
 import { FiEdit } from "react-icons/fi";
+import { BiCommentDetail } from "react-icons/bi";
+import { BsJournalBookmark } from "react-icons/bs";
+import { HiOutlinePhotograph } from "react-icons/hi";
 
 const Layout = ({ children }) => {
+  const pages = [
+    { icon: <HiOutlinePhotograph />, page: "Posts" },
+    { icon: <BiCommentDetail />, page: "Activity" },
+    { icon: <BsJournalBookmark />, page: "Saved Posts" },
+  ];
   return (
     <div style={{ padding: "30px 140px" }}>
       <section>
@@ -27,6 +37,24 @@ const Layout = ({ children }) => {
           />
           <Typography variant="h2">Hello</Typography>
           <FiEdit />
+        </Paper>
+        <Paper
+          sx={{
+            background: "#fff",
+            width: "200px",
+            height: "200px",
+          }}
+        >
+          <List>
+            {pages.map((page, index) => {
+              return (
+                <ListItem key={index}>
+                  {page.icon}
+                  <Typography variant="body1">{page.page}</Typography>
+                </ListItem>
+              );
+            })}
+          </List>
         </Paper>
       </section>
     </div>
