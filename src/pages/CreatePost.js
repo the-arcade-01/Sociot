12259eb0051cd9299a07a74916e01fa8567f95dc
the-arcade-01 <aashisharcade@gmail.com>
2022-01-26 00:27:31 +0530

    createpost started

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+
 const CreatePost = () => {
   return (
-    <div>
-      <h1>Create Post</h1>
-    </div>
+    <Paper
+      sx={{
+        background: "#132c33",
+        padding: "20px 40px",
+        height: "500px",
+        width: "780px",
+      }}
+    >
+      <Typography variant="h4" sx={{ fontWeight: "500", color: "#d8e3e7" }}>
+        Create Post
+      </Typography>
+    </Paper>
   );
 };
 
