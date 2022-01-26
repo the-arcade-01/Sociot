c49b5b5e8727ae67f40753f69176d3eb51e69c01
the-arcade-01 <aashisharcade@gmail.com>
2022-01-26 20:16:18 +0530

    title category added

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -2,8 +2,23 @@ import React from "react";
 
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 const CreatePost = () => {
+  const categories = [
+    "Music",
+    "Funny",
+    "Videos",
+    "Programming",
+    "News",
+    "Fashion",
+  ];
+
   return (
     <Paper
       sx={{
@@ -16,6 +31,89 @@ const CreatePost = () => {
       <Typography variant="h4" sx={{ fontWeight: "500", color: "#d8e3e7" }}>
         Create Post
       </Typography>
+      <form autoComplete="off" style={{ margin: "30px 40px" }}>
+        <div
+          style={{
+            display: "flex",
+            gap: "95px",
+          }}
+        >
+          <InputLabel>
+            <Typography
+              variant="h6"
+              sx={{ color: "#d8e3e7", marginTop: "10px" }}
+            >
+              Title
+            </Typography>
+          </InputLabel>
+          <TextField
+            sx={{
+              background: "#d8e3e7",
+              color: "#132c33",
+              fontWeight: "500",
+              height: "55px",
+              width: "400px",
+              borderRadius: "10px",
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+              "&:hover .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+              "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+              "& .Mui-disabled .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+            }}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            gap: "40px",
+            marginTop: "20px",
+          }}
+        >
+          <Typography variant="h6" sx={{ color: "#d8e3e7" }}>
+            Categories
+          </Typography>
+          <Box sx={{ background: "#126e82", borderRadius: "5px" }}>
+            <Select
+              sx={{
+                border: "none",
+                width: "100px",
+                height: "50px",
+                borderRadius: "5px",
+                color: "#d8e3e7",
+                fontWeight: "500",
+                minWidth: "100px",
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: "none",
+                },
+                "&:hover .MuiOutlinedInput-notchedOutline": {
+                  border: "none",
+                },
+                "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                  border: "none",
+                },
+                "& .Mui-disabled .MuiOutlinedInput-notchedOutline": {
+                  border: "none",
+                },
+              }}
+            >
+              {categories.map((cate, index) => {
+                return (
+                  <MenuItem key={index} value={`${cate}.toLowerCase()`}>
+                    {cate}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </Box>
+        </div>
+      </form>
     </Paper>
   );
 };
