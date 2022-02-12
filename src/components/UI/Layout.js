410b390b2435d8b0d3fc7b4c4c3fb7e047cb539a
the-arcade-01 <aashisharcade@gmail.com>
2022-02-12 11:01:12 +0530

    footer added

--- src/components/UI/Layout.js
+++ src/components/UI/Layout.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import Footer from "./Footer";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 const Layout = () => {
   return (
-    <div>
-      <h1>Layout</h1>
+    <Box component="div">
+      <Typography variant="h4">Layout</Typography>
       <Outlet />
-    </div>
+      <Footer />
+    </Box>
   );
 };
 
