5357e09c7484f674a9d1fce33ea3c9235469054b
Aashish Koshti <aashisharcade@gmail.com>
2023-03-04 23:29:57 +0530

    initial commit v2

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/home/the-arcade-01/Documents/Sociot/app/page": [
+      "/home/the-arcade-01/Documents/Sociot/app/globals.css"
+    ]
+  },
+  "/home/the-arcade-01/Documents/Sociot/app/layout.tsx": [
+    "/home/the-arcade-01/Documents/Sociot/app/globals.css"
+  ]
+}
